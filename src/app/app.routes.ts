fd01dfbdde469e73a0c7bec8b69d94b8f3dde4b0
sameendraK <sameendrak@gmail.com>
2024-09-19 09:12:32 +0530

    WIP: (app.component,app-routes): Add routing, need to change the onActiveItemChange function internals.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,30 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
+import { ProductsComponent } from './components/products/products.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
-    }
+    },
+    {
+        path: 'Dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'Transactions',
+        component: TransactionsComponent
+    },
+    {
+        path: 'Products',
+        component: ProductsComponent
+    },
+    {
+        path: 'Messages',
+        component: MessagesComponent
+    },
 ];
