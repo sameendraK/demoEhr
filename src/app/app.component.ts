b103c92a2333d8e322d944753331ee1ada3a9c0c
sameendraK <sameendrak@gmail.com>
2024-09-30 12:15:53 +0530

    fix: fix the styling using seperate containers for header bar and router outlet (app.component)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { SchedulerModule } from 'angular-calendar-scheduler';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent],
+  imports: [RouterOutlet, HomeComponent, SchedulerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
